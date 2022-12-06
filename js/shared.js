8cebf8c7a318431801d351ab00c93a57a6ac6e7b
Oliver Hardisty <o.hardisty@me.com>
2022-12-06 21:16:27 +0000

    Update shared.js

--- js/shared.js
+++ js/shared.js
@@ -18,7 +18,9 @@ $(function(){
       $(".navigation-html").load("html/shared/navigation.html"); 
     });
 
-
+$(function(){
+      $(".navigation-html").load("shared/navigation.html"); 
+    });
 
 $(function(){
   $(".project-picker-html").load("html/shared/project-picker.html"); 
