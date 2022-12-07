72b9c0f5befd798f30b308df78baa39c32cd161d
Oliver Hardisty <o.hardisty@me.com>
2022-12-07 11:04:03 +0000

    Update shared.js

--- js/shared.js
+++ js/shared.js
@@ -32,8 +32,8 @@ $(function(){
 
 
 $(document).ready(function () {
-              $(".navigation > li").click(function (e) {
-               $(".navigation > li").removeClass(".text_red");
+              $(".navigation > h2").click(function (e) {
+               $(".navigation > h2").removeClass(".text_red");
                $(this).addClass(".text_red");
                 });
             });
