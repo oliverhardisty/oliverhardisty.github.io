07e324e934d057ee02cd7baabef0e46b1f44286c
Oliver Hardisty <o.hardisty@me.com>
2022-12-14 16:58:24 +0000

    Update home.js

--- js/home.js
+++ js/home.js
@@ -40,7 +40,7 @@ $(window).scroll(function () {
         }
     });
     $('.arrow').each(function () {
-        if (($(this).offset().top - $(window).scrollTop())) {
+        if (($(this).offset().top - $(window).scrollTop())< -100) {
             $(this).stop().fadeTo(100, 0);
         } else {
             $(this).stop().fadeTo('slow', 1);
