1d564743537c7719aa14bf587a0b32132397d250
Oliver Hardisty <o.hardisty@me.com>
2023-07-25 18:26:44 +0300

    Update home.js

--- js/home.js
+++ js/home.js
@@ -57,7 +57,7 @@ $(window).scroll(function () {
     });
     $('.gradient-bottom').each(function () {
         if (($(this).offset().top - $(window).scrollTop()) < - 250) {
-            $(this).stop().fadeTo(100, 0);
+            $(this).stop().fadeTo(0, 100);
         } else {
             $(this).stop().fadeTo('fast', 1);
         }
