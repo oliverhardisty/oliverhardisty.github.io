05c9015242c5f46333ea2b127bfbd7d8693a0025
Oliver Hardisty <o.hardisty@me.com>
2023-07-25 14:11:17 +0300

    Update home.js

--- js/home.js
+++ js/home.js
@@ -54,6 +54,11 @@ $(window).scroll(function () {
         } else {
             $(this).stop().fadeTo('fast', 1);
         }
+        if (($(this).offset().top - $(window).scrollTop()) > 600) {
+            $(this).stop().fadeTo(0, 100);
+        } else {
+            $(this).stop().fadeTo('fast', 1);
+        }
     });
 });
 
