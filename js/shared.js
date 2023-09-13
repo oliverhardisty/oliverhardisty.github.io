0614d45f204f434931afa34043f7d10544b56fcf
Oliver Hardisty <o.hardisty@me.com>
2023-09-13 14:11:03 +0300

    Update shared.js

--- js/shared.js
+++ js/shared.js
@@ -49,7 +49,15 @@ $(function(){
 
 
 
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar_fixed").style.top = "0";
+  } else {
+    document.getElementById("navbar_fixed").style.top = "-50px";
+  }
+}
 
 
 
