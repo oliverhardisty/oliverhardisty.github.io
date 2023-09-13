ad8d758039744b400aff2b4083c82a35892105bb
Oliver Hardisty <o.hardisty@me.com>
2023-09-13 14:10:49 +0300

    Update project.js

--- js/project.js
+++ js/project.js
@@ -73,16 +73,6 @@ $("#infobutton6").click(function(){
     });
  
    
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar_fixed").style.top = "0";
-  } else {
-    document.getElementById("navbar_fixed").style.top = "-50px";
-  }
-}
   
   
  
