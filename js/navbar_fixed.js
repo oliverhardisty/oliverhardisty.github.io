759e44986f74cc399685609d4e189985a1572002
Oliver Hardisty <o.hardisty@me.com>
2023-09-14 14:21:04 +0300

    Delete js/navbar_fixed.js

--- js/navbar_fixed.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 2500 || document.documentElement.scrollTop > 2500) {
-    document.getElementById("navbar_fixed").style.top = "0";
-  } else {
-    document.getElementById("navbar_fixed").style.top = "-56px";
-  }
-}
