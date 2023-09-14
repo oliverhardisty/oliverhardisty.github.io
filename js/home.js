d9f77e63777aa0c7280ca268ea76c68a14bd101b
Oliver Hardisty <o.hardisty@me.com>
2023-09-14 14:12:30 +0300

    Update home.js

--- js/home.js
+++ js/home.js
@@ -9,7 +9,7 @@ $(window).scroll(function() {
   
   
   // Change 33% earlier than scroll position so colour is there when you arrive.
-  var scroll = $window.scrollTop() + 600;
+  var scroll = $window.scrollTop() + 650;
  
   $panel.each(function () {
     var $this = $(this);
